f9fd5b737c5eb9a5bf7d595bd10ad4d7e9f0cd27
thefortob <aoberlin@thefortaiagency.com>
2025-11-11 10:21:53 -0500

    Disable React StrictMode - double-mounting breaks Three.js texture loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// StrictMode disabled - causes double-mounting which breaks Three.js texture loading
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
